a022e963a4e4e91cccfde270a34491c5be06ccfd
Edwin Watkeys <edw@poseur.com>
2015-10-12 10:18:15 -0400

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thunknyc/adtech-macros "0.1.1"
+(defproject thunknyc/adtech-macros "0.1.2"
   :description "A Clojure library designed to do adtech-style (aka UNIX-style aka DOS-style) macros aka string interpolation."
   :url "http://github.com/thunknyc/adtech-macros"
   :license {:name "Eclipse Public License"
