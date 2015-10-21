9bbf677b02230fa3e5cba4e11a809dee6ea82290
Edwin Watkeys <edw@poseur.com>
2015-10-21 16:14:49 -0400

    reimplement

--- src/adtech/macros/core.clj
+++ src/adtech/macros/core.clj
@@ -1,6 +1,5 @@
 (ns adtech.macros.core
   (:require [clojure.string :as string]
-            [clojure.zip :as zip]
             [instaparse.core :refer [parser]]))
 
 (def ^:dynamic *registered-filters*
@@ -9,17 +8,6 @@
    :capitalize string/capitalize})
 
 (def ^:dynamic *filters* [#(java.net.URLEncoder/encode %)])
-
-(defn- apply-filters
-  [tree v]
-  (let [tree-filters (:filters (meta tree))
-        filters (->> (concat tree-filters *filters*)
-                     (map #(or (*registered-filters* %) %)))]
-    (reduce #(%2 %1) v filters)))
-
-(defn- string->path
-  [path coll]
-  [(keyword path)])
 
 (def path-parser
   (parser
@@ -30,99 +18,105 @@
    <K> = #'[-_:0-9a-zA-Z/*?<>]+'
    <E> = K | <'('> S <')'>"))
 
-(defn- tree-has-filters?
+(defn- macro-spec?
   [t]
-  (= :FS (first (nth t 1))))
+  (and (vector? t) (> (count t) 0) (= :S (nth t 0))))
 
-(defn- tree-has-backup?
+(defn- backup-spec?
   [t]
-  (= :BU (first (last t))))
+  (and (vector? t) (> (count t) 0) (= :BU (nth t 0))))
 
-(defn- tree-filters
+(defn- filters-spec?
   [t]
-  (->> (get t 1)
-       (rest)
-       (map keyword)))
+  (and (vector? t) (> (count t) 0) (= :FS (nth t 0))))
 
-(defn- tree?
+(declare ^:private make-macro-spec)
+
+(defn- make-backup-spec
+  [coll]
+  (make-macro-spec (nth coll 1)))
+
+(defn- make-filters-spec
+  [coll]
+  (map keyword (rest coll)))
+
+(defn- make-macro-spec
   [t]
-  (and (vector? t) (= :S (first t))))
+  (if (macro-spec? t)
+    (loop [t (rest t) spec {}]
+      (if (seq t)
+        (let [el (first t)]
+          (cond (backup-spec? el)
+                (recur (rest t) (assoc spec :backup (make-backup-spec el)))
+                (filters-spec? el)
+                (recur (rest t) (assoc spec :filters (make-filters-spec el)))
+                (macro-spec? el)
+                (recur (rest t) (update-in spec [:path] (fnil conj []) (make-macro-spec el)))
+                (string? el)
+                (recur (rest t) (update-in spec [:path] (fnil conj []) el))))
+        spec))))
 
-(defn- clean-tree
-  [t]
-  (cond (not (tree? t))
-        t
-        (tree-has-filters? t)
-        (with-meta (map clean-tree (nthrest t 2)) {:filters (tree-filters t)})
-        :else
-        (map clean-tree (rest t))))
+(declare ^:private replace-macro)
 
-(defn- extract-backup [t]
-  (if (tree-has-backup? t)
-    [(with-meta (butlast t) (meta t)) (clean-tree (nth (last t) 1))]
-    [t nil]))
-
-(defn- parse-path
-  [path]
-  (when path
-    (let [tree (path-parser path)]
-      (clean-tree tree))))
-
-(declare ^:private resolve-path)
-
-(defn- resolve-path-elem
-  [coll elem default]
-  (if (sequential? elem)
-    (binding [*filters* nil] (resolve-path coll elem nil))
-    elem))
-
-(defn- ref? [x]
-  (instance? clojure.lang.IDeref x))
-
-(defn- normalize-value [v]
-  (cond (or (keyword? v) (symbol? v))
+(defn- cleanse
+  [v]
+  (cond (string? v)
+        v
+        (or (instance? java.lang.Boolean v) (number? v))
+        (str v)
+        (or (keyword? v) (symbol? v))
         (name v)
         :else
-        (str v)))
+        nil))
 
-(defn- return-missing
-  [coll tree backup default]
-  (if backup
-    (apply-filters
-     tree
-     (normalize-value (resolve-path coll backup default)))
-    default))
+(defn- filter-value
+  [fs v]
+  (reduce (fn [v f] (f v))
+          v
+          (->> (concat fs *filters*)
+               (map #(or (*registered-filters* %) %)))))
 
-(defn- resolve-path
-  ([coll tree default]
-   (let [[tree bu] (extract-backup tree)]
-     (loop [sub-coll coll els tree]
-       (if (and sub-coll (seq els))
-         (let [el (resolve-path-elem coll (first els) default)]
-           (cond (nil? el)
-                 (return-missing coll tree bu default)
-                 (sequential? sub-coll)
-                 (let [idx (try (Integer/parseInt el)
-                                (catch NumberFormatException e (return-missing coll tree bu default)))]
-                   (if (contains? sub-coll idx)
-                     (recur (get sub-coll (Integer/parseInt el)) (rest els))
-                     (return-missing coll tree bu default)))
-                 (map? sub-coll)
-                 (let [el-kw (keyword el)]
-                   (cond (contains? sub-coll el-kw)
-                         (recur (get sub-coll el-kw) (rest els))
-                         (contains? sub-coll el)
-                         (recur (get sub-coll el) (rest els))
-                         :else
-                         (return-missing coll tree bu default)))
-                 :else
-                 (return-missing coll tree bu default)))
-         (if (or (sequential? sub-coll) (map? sub-coll))
-           (return-missing coll tree bu default)
-           (apply-filters tree (normalize-value sub-coll))))))))
+(defn- finish-value
+  [filters coll value missing backup]
+  (if-let [v (cleanse value)]
+    (filter-value filters v)
+    (if backup
+      (replace-macro backup coll missing)
+      (filter-value filters missing))))
+
+(defn- replace-macro
+  [{:keys [filters backup path] :as tree} coll missing]
+  (loop [path path subcoll coll]
+    (if (seq path)
+      (let [raw-el (first path)
+            el (if (map? raw-el)
+                 (binding [*filters* nil] (replace-macro raw-el coll nil))
+                 raw-el)
+            val (cond (nil? el)
+                      nil
+                      (map? subcoll)
+                      (or (get subcoll (keyword el))
+                          (get subcoll el))
+                      (sequential? subcoll)
+                      (let [idx (try (Integer/parseInt el)
+                                     (catch NumberFormatException e
+                                       nil))]
+                        (when (contains? subcoll idx)
+                          (nth subcoll idx)))
+                      :else
+                      nil)]
+        (if val
+          (recur (rest path) val)
+          (finish-value filters coll val missing backup)))
+      (finish-value filters coll subcoll missing backup))))
 
 (defn render
   ([s coll] (render s coll ""))
-  ([s coll default]
-   (string/replace s #"\$\{\s*([^}]+?)\s*\}"
-                   (fn [[_ k]] (resolve-path coll (parse-path k) default)))))
+  ([s coll missing]
+   (string/replace
+    s
+    #"\$\{\s*([^}]+?)\s*\}"
+    (fn [[_ macro]]
+      (-> (path-parser macro)
+          make-macro-spec
+          (replace-macro coll missing))))))
